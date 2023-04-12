1939b402f40d63a760f4e4f6a67041e4ad977d66
ssanchez32 <ssanchez32@unm.edu>
2023-04-12 11:05:44 -0600

    Grid

--- Scripts/02_Data_Exploration.R
+++ Scripts/02_Data_Exploration.R
@@ -15,6 +15,7 @@ library("ggmap")
 #### load data ####
 
 dat = read.csv("Outside_Data /SantaFeMonthlyDataExport_050920224.csv" , header = TRUE)
+
 options(scipen = 999)
 
 ####Data Exploration####
@@ -50,35 +51,23 @@ dat_sub <-
     , Total_Comsumption_Gal               = Total.Consumption..gal.
    )
 
-#Zone 3 data ONLY
-dat_Zone3 = dat_sub %>%
-  filter(Zone == "Zone3") %>%
-  select(Month, Year, Total_Comsumption_Gal)
+####Water use by zone ONLY####
+dat_zone00 = dat_sub[dat_sub$Zone=="ZONE 00",]
+dat_zone0 = dat_sub[dat_sub$Zone=="ZONE 0",]
+dat_zone1 = dat_sub[dat_sub$Zone=="ZONE 1",]
+dat_zone2 = dat_sub[dat_sub$Zone=="ZONE 2",]
+dat_zone3 = dat_sub[dat_sub$Zone=="ZONE 3",]
+dat_zone4 = dat_sub[dat_sub$Zone=="ZONE 4",]
+dat_zone5 = dat_sub[dat_sub$Zone=="ZONE 5",]
+dat_zone6 = dat_sub[dat_sub$Zone=="ZONE 6",]
+dat_zone7 = dat_sub[dat_sub$Zone=="ZONE 7",]
+dat_zone8 = dat_sub[dat_sub$Zone=="ZONE 8",]
+dat_zone9 = dat_sub[dat_sub$Zone=="ZONE 9",]
 
-#### format date/time ####
-strptime
-
-#linelist <- linelist %>% 
-  #mutate(onset_date = make_date(year = onset_year, month = onset_month, day = onset_day))
-
-####test date formatting ####
-#do not use (yet!)
-#as.Date(paste(YYYY, MM, "01", sep("-")))
-#as.Date(paste(YYYY, MM, "%b/%Y"))
-
-#tail(dat$Sample_DateTime)
-#tail(as.POSIXct(dat$Sa /'mple_DateTime, format= "%m/%d/%y %H:%M", tz="MST"))
-
-#create new date/time col with correct format 
-#dat$datetime_MST = as.POSIXct(dat$Sample_DateTime, format= "%m/%d/%y %H:%M", tz="MST")
-
-#### plotting #####
-
-#this code will plot the entire data set by zone (also takes an extremely long time)
-#plot(dat_sub)
-
-Zones <- ggplot(data = dat_sub, aes(x=Month, y=Total_Comsumption_Gal))+
+#####Graphing plots by Zone####
+Zones <- ggplot(data = dat_zones, aes(x=Month, y=Total_Comsumption_Gal))+
   geom_point()+
+  geom_boxplot()+
   stat_summary(
    geom = "point",
     fun = "mean",
@@ -90,9 +79,34 @@ Zones <- ggplot(data = dat_sub, aes(x=Month, y=Total_Comsumption_Gal))+
 print(Zones)
 ggsave("Zones.png", plot = Zones)
 
-#plotting by zone
+####Plotting by different zones####
+Zone00 <- ggplot(data = dat_zone00, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone00")
+print(Zone00)
+ggsave("Zone00.png", plot = Zone00)
 
-Zone1 <- ggplot(data = dat_sub, aes(x=Year, y=Total_Comsumption_Gal))+
+Zone0 <- ggplot(data = dat_zone0, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone0")
+print(Zone0)
+ggsave("Zone0.png", plot = Zone0)
+
+Zone1 <- ggplot(data = dat_zone1, aes(x=Year, y=Total_Comsumption_Gal))+
   geom_point()+
   stat_summary(
     geom = "point",
@@ -105,7 +119,7 @@ Zone1 <- ggplot(data = dat_sub, aes(x=Year, y=Total_Comsumption_Gal))+
 print(Zone1)
 ggsave("Zone1.png", plot = Zone1)
 
-Zone2 <- ggplot(data = dat_sub, aes(x=Year, y=Total_Comsumption_Gal))+
+Zone2 <- ggplot(data = dat_zone2, aes(x=Year, y=Total_Comsumption_Gal))+
   geom_point()+
   stat_summary(
     geom = "point",
@@ -118,9 +132,8 @@ Zone2 <- ggplot(data = dat_sub, aes(x=Year, y=Total_Comsumption_Gal))+
 print(Zone2)
 ggsave("Zone2.png", plot = Zone2)
 
-#plotting zone 3 due to the interesting story shown versus the other water delivery pressure zones
 
-Zone3 <- ggplot(data = dat_sub, aes(x=Year, y=Total_Comsumption_Gal))+
+Zone3 <- ggplot(data = dat_zone3, aes(x=Year, y=Total_Comsumption_Gal))+
   geom_point()+
   stat_summary(
     geom = "point",
@@ -133,7 +146,96 @@ Zone3 <- ggplot(data = dat_sub, aes(x=Year, y=Total_Comsumption_Gal))+
 print(Zone3)
 ggsave("Zone3.png", plot = Zone3)
 
-table(dat$Site_Name)
+Zone4 <- ggplot(data = dat_zone4, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone4")
+print(Zone4)
+ggsave("Zone4.png", plot = Zone4)
+
+Zone5 <- ggplot(data = dat_zone5, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone5")
+print(Zone5)
+ggsave("Zone5.png", plot = Zone5)
+
+Zone6 <- ggplot(data = dat_zone6, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone6")
+print(Zone6)
+ggsave("Zone6.png", plot = Zone6)
+
+Zone7 <- ggplot(data = dat_zone7, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone7")
+print(Zone7)
+ggsave("Zone7.png", plot = Zone7)
+
+Zone8 <- ggplot(data = dat_zone8, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone8")
+print(Zone8)
+ggsave("Zone8.png", plot = Zone8)
+
+Zone9 <- ggplot(data = dat_zone9, aes(x=Year, y=Total_Comsumption_Gal))+
+  geom_point()+ 
+  stat_summary(
+    geom = "point",
+    fun = "mean",
+    col = "black",
+    size = 3,
+    shape = 24,
+    fill = "red") +
+  facet_wrap(~"Zone9")
+print(Zone9)
+ggsave("Zone9.png", plot = Zone9)
+
+####Grid plot####
+library(gridExtra)
+grid_zones<-grid.arrange(grobs = list(Zone00, Zone0, Zone1, Zone2, Zone3, Zone4, Zone5, Zone6, Zone7, Zone8, Zone9), nrow=4)
+ggsave("grid_zones.png", plot = grid_zones)
+
+####nlme model####
+
+
+
+
+
+
 
 ####plotting map####
 register_google(key = "AIzaSyAEUCxlN7Nvje2a24PW065bblJsCNH3ekg") 
@@ -152,43 +254,26 @@ print(sfmapsat)
 sfmap <-ggmap(get_googlemap("Santa Fe", zoom = 12, maptype = "terrain"))
 print(sfmap)
 
-####shapefile overlay####
-
+####Shapefile Overlay####
 install.packages("rinat")
 library(rinat)
 library(rgdal)
 library(sf)
 library(grid)
 library(ggmap)
-
-#sfshape <-readOGR("Shape_File_Data/WaterPressureZone.shp")
-#print(sfshape)
+library(terra)
+library(tidyterra)
 
 aoi_boundary_SF <- st_read(
   "Shape_File_Data/WaterPressureZone.shp")
 
 WPZ <- ggplot() + 
-  geom_sf(data = aoi_boundary_SF, size = 3, color = "black", fill = "cyan", aes(x=lon, y=lat)) + 
+  geom_sf(data = aoi_boundary_SF, size = 3, color = "black", fill = "cyan") + 
   ggtitle("Water Pressure Zones in the city of Santa fe") + 
   coord_sf() 
 print(WPZ) 
-ggsave("WPZ")
 
-shape_overlay <- print(sfmap) +
-  geom_sf(data = aoi_boundary_SF, size = 3, color = "black", fill = "cyan1") + 
-  ggplot(WPZ) + 
-  ggtitle("WaterPressureZone") + 
-  coord_sf() 
+shape_overlay<-ggplot() + geom_spatraster(data=mapRaster) + geom_spatvector(data=aoi_boundary_spatve, fill="red", lab="Zones")
+print(shape_overlay)
 
-ggmap(get_googlemap("Santa Fe", maptype = "terrain")) + 
-  geom_sf(data = aoi_boundary_SF, inherit.aes = FALSE, fill="blue") +
-  coord_sf()
-
-####grid plot####
-
-#library(gridExtra)
-#grid.arrange(grobs = list(Zone2, Zone3), nrow=2)
-
-
-
-ggsave("~/Desktop/2023-2024/Stakeholder Driven Analysis/Santa_Fe_Water_Data/Inside_Data")
+ggsave("~/Desktop/Stakeholder Driven Analysis/Santa_Fe_Water_Data/Inside_Data")
